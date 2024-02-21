59b3d05ddbf6043ec96d32e0add10ee1b47ac3b1
cynthia <61641064+cor100@users.noreply.github.com>
2024-02-21 10:52:28 +0100

    menu

---
+++ Assets/Scenes/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer gravityIconRenderer;
+    [SerializeField] private SpriteRenderer characterRenderer;
+    public SpriteRenderer[] characters;
+    public SpriteRenderer currentChar;
+    void Start(){
+        gravityIconRenderer = gameObject.GetComponent<SpriteRenderer>();
+        currentChar = gameObject.GetComponent<SpriteRenderer>();
+    }
+    public void restart(){
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex +1;
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void toggleGravityIcon(){
+        gravityIconRenderer.enabled = !gravityIconRenderer.enabled;
+    }
+    public void purple(){
+        currentChar = characters[2];
+    }
+    public void blue(){
+        currentChar = characters[1];
+
+    }
+    public void owl(){
+        currentChar = characters[0];
+    }
+
+    public void menuSetActive(){
+        gameObject.SetActive(!gameObject.activeSelf);
+    } 
+}
