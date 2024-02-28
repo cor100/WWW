b1c8f224d18a5dad38c4d78e4e45c9017a74e854
cynthia <61641064+cor100@users.noreply.github.com>
2024-02-28 16:47:44 +0100

    changes character upon button press

--- Assets/Scenes/Scripts/Menu.cs
+++ Assets/Scenes/Scripts/Menu.cs
@@ -1,15 +1,28 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer gravityIconRenderer;
     [SerializeField] private SpriteRenderer characterRenderer;
-    public SpriteRenderer[] characters;
-    public SpriteRenderer currentChar;
+    public GameObject purpleMonst;
+    public GameObject blueMonst;
+    public GameObject owlMonst;
+    public GameObject startChar;
+
+    private Vector2 currentLocation;
+    public static GameObject currentChar;
+
     void Start(){
         gravityIconRenderer = gameObject.GetComponent<SpriteRenderer>();
-        currentChar = gameObject.GetComponent<SpriteRenderer>();
+        if(!currentChar){
+            currentChar = owlMonst;
+        }
+    }
+
+    void Update(){
+        currentLocation = currentChar.transform.position;
     }
     public void restart(){
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -19,14 +32,25 @@ public class Menu : MonoBehaviour
         gravityIconRenderer.enabled = !gravityIconRenderer.enabled;
     }
     public void purple(){
-        currentChar = characters[2];
+        blueMonst.SetActive(false);
+        owlMonst.SetActive(false);
+        purpleMonst.transform.position = currentLocation;
+        purpleMonst.SetActive(true);
+        currentChar = purpleMonst;
     }
     public void blue(){
-        currentChar = characters[1];
-
+        blueMonst.SetActive(true);
+        blueMonst.transform.position = currentLocation;
+        owlMonst.SetActive(false);
+        purpleMonst.SetActive(false);
+        currentChar = blueMonst;
     }
     public void owl(){
-        currentChar = characters[0];
+        blueMonst.SetActive(false);
+        owlMonst.SetActive(true);
+        owlMonst.transform.position = currentLocation;
+        purpleMonst.SetActive(false);
+        currentChar = owlMonst;
     }
 
     
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,12 +6,6 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject menu;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
