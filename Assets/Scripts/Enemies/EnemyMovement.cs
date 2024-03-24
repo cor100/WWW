a5a42c1c699f8495afbc5902a8f324bc0c06016a
Hannah Guo <hannah.guo123@gmail.com>
2024-03-24 23:37:13 +0100

    wip getting enemy to move don't merge yet maybe

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -4,33 +4,46 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Transform enemyTransform;
-
     // positions relative to the original position
-    [SerializeField] private List<Vector2> wayPoints;
+    [SerializeField] private List<Vector3> wayPoints;
     private int wayPointsIndex = 0;
     private int wayPointsLength;
 
-    [SerializeField] private int moveSpeed;
+    //[SerializeField] private int moveSpeed;
     // When the distance between Enemy Pos & Target Pos is less than arrivalThreshold, consider Enemy Pos to be at Target Pos
     [SerializeField] private float arrivalThreshold;
     [SerializeField] private float waitingSecondsPerPosition;
+    [SerializeField] private float moveDistancePerFrame;
 
-    //private void OnDrawGizmosSelected()
-    //{
-    //    Gizmos.DrawSphere();
-    //}
+    private Vector3 nextTargetPosition;
+    private bool arrivedAtNextPos = false;
+    private bool isAlive = true;
+
+    private void OnDrawGizmosSelected()
+    {
+        //for(int i = 0; i < (wayPointsIndex - 1); i++)
+        //{
+
+        //}
+        Gizmos.DrawSphere(wayPoints[0], (float) 0.1);
+        Gizmos.DrawSphere(wayPoints[1], (float) 0.1);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        wayPointsLength = wayPoints.Count;
+        wayPointsLength = wayPoints.Count - 1;
+        GetNextTargetPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<Transform>().position = 
+        StartCoroutine(IMovingCoroutine());
+        if (arrivedAtNextPos)
+        {
+            Debug.Log("arrived from update");
+        }
     }
 
     // method movingCoroutine
@@ -38,11 +51,36 @@ public class EnemyMovement : MonoBehaviour
         // while distance between target position & my position > arrival threshold, move
         // otherwise, wait
         // use vector3.movetowards()
+    private IEnumerator IMovingCoroutine()
+    {
+        if (isAlive)
+        {
+            if (!arrivedAtNextPos)
+            {
+                float withinArrivalThreshold = (transform.position - nextTargetPosition).magnitude;
+                if (withinArrivalThreshold > arrivalThreshold)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, nextTargetPosition, moveDistancePerFrame);
+                }
+                else
+                {
+                    yield return new WaitForSeconds(waitingSecondsPerPosition);
+                    arrivedAtNextPos = true;
+                    Debug.Log("arrived from coroutine");
+                }
+            }
+            else
+            {
+                GetNextTargetPosition();
+                arrivedAtNextPos = false;
+            }
+        }
+    }
 
     // method getting next target position, loop through the list & return
-    Vector2 GetNextTargetPosition()
+    private void GetNextTargetPosition()
     {
-        Vector2 targetPosition = wayPoints[wayPointsIndex];
+        Vector3 targetPosition = wayPoints[wayPointsIndex];
         if(wayPointsIndex >= wayPointsLength)
         {
             wayPointsIndex = 0;
@@ -52,7 +90,7 @@ public class EnemyMovement : MonoBehaviour
             wayPointsIndex += 1;
         }
 
-        return targetPosition;
+        nextTargetPosition = targetPosition;
     }
 
     // on draw gizmo selected
