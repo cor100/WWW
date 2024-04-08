fff7040266ae6fbc4f544cbe3b8ace5c8b35c36f
cynthia <61641064+cor100@users.noreply.github.com>
2024-04-08 17:46:01 +0200

    camera follow works + start does not quite work

--- Assets/Scripts/GameManagement/SceneLoader.cs
+++ Assets/Scripts/GameManagement/SceneLoader.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SceneLoader : MonoBehaviour
 {
+  [SerializeField] private string nextSceneName;
+
   // Quits application when clicked
   public void QuitGame()
     {
         Application.Quit();
     }
-   
+  
+  public void StartGame()
+    {
+        //PlayerPrefs.SetInt("selectedCharacter", CharacterSelect.selectedCharacter);
+        SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+
+    }
 }
