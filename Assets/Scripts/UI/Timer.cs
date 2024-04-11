40802845b75e7c70184a7c83d31edfdef36f457f
Aldo G. Ruiz Parra <aldoruizparra@gmail.com>
2024-04-11 21:18:24 +0200

    added camera shake, changed timer

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
     public float timeRemaining = 10;
     public TextMeshProUGUI timerText;
-    private bool _timerIsRunning = false; 
+    private bool _timerIsRunning = false;
+    private bool _hasNotSetOffShake = true;
     // Start is called before the first frame update
     void Start()
     {
         _timerIsRunning = true;
+
     }
 
     // Update is called once per frame
@@ -22,12 +25,21 @@ public class Timer : MonoBehaviour
         if(_timerIsRunning){
             if (timeRemaining > 0){
                 timeRemaining -= Time.deltaTime;
+                Console.WriteLine(timeRemaining);
                 DisplayTime(timeRemaining);
             }else{
                 timeRemaining = 0;
                 _timerIsRunning = false;
+                _hasNotSetOffShake = true;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             
+            }
+
+            // This sets off shaking at the 5 second timer
+            if (timeRemaining <= 5 && _hasNotSetOffShake)
+            {
+                CameraShake.Instance.ShakeCamera(1f, .5f);
+                _hasNotSetOffShake = false;
             }
         } 
     }
