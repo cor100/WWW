9e984fec6e145eb1580bc47b14e6a3d338561ed1
cynthia <61641064+cor100@users.noreply.github.com>
2024-04-24 23:03:34 +0200

    animate coin

---
+++ Assets/CoinAnimator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+    private SpriteRenderer _coinSpriteRenderer;
+    private bool _isCoroutineRunning;
+    private float animateDelay = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coinSpriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(animateCoin());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        
+    }
+    IEnumerator animateCoin(){
+      int frameIndex = 0;
+      while(true){
+        _coinSpriteRenderer.sprite = sprites[frameIndex];
+        frameIndex++;
+        if(frameIndex >= sprites.Length){
+                frameIndex = 0;
+            }
+            yield return new WaitForSeconds(animateDelay);
+        }
+    }
+    
+}
